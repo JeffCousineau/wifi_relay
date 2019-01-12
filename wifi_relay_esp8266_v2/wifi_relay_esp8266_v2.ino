80557fd72687ef4f975be53d7211c8c8246d1c21
Jeffrey Cousineau <jeffrey.cousineau@gmail.com>
2019-01-11 22:33:37 -0500

    Add POST request with delay argument

--- wifi_relay_esp8266_v2/wifi_relay_esp8266_v2.ino
+++ wifi_relay_esp8266_v2/wifi_relay_esp8266_v2.ino
@@ -3,16 +3,25 @@
  
 ESP8266WebServer server(80);
  
-const char* ssid = "YourNetworkName";
-const char* password =  "YourNetworkPassword";
+const char* ssid = "";
+const char* password =  "";
 const char* api_key = "";
 
+// Auxiliar variables to store the current output state
+String output5State = "off";
+// Assign output variables to GPIO pins
+const int ledPin = 13;
+
+int door_delay = 4000;
  
 void setup() {
  
     Serial.begin(115200);
     WiFi.begin(ssid, password);  //Connect to the WiFi network
- 
+    // Initialize the output variables as outputs
+    pinMode(ledPin, OUTPUT);
+    // Set outputs to LOW
+    digitalWrite(ledPin, HIGH);
     while (WiFi.status() != WL_CONNECTED) {  //Wait for connection
  
         delay(500);
@@ -23,8 +32,7 @@ void setup() {
     Serial.print("IP address: ");
     Serial.println(WiFi.localIP());  //Print the local IP
  
-    server.on("/body", handleBody); //Associate the handler function to the path
-    server.on("/api", handleApiArg);   //Associate the handler function to the path
+    server.on("/open", handleOpenArg);   //Associate the handler function to the path
     
     server.begin(); //Start the server
     Serial.println("Server listening");
@@ -35,23 +43,34 @@ void loop() {
     server.handleClient(); //Handling of incoming requests
 }
  
-void handleApiArg() { 
+void handleOpenArg() { 
 
 String message = "";
 
-if (server.arg("api-key")== ""){     //Parameter not found
+if (server.arg("api_key")== ""){     //Parameter not found
   message = "No API key";
 }
 else{     //Parameter found
-  if (server.arg("api-key") == api_key)
+  if (server.arg("api_key") == api_key)
   {
-    message = "Door opening...";   
+    if (server.arg("door_delay")!= "")
+    {
+      door_delay = server.arg("door_delay").toInt();
+    }
+    else
+    {
+      door_delay = 4000;
+    }
+    message = "Door opening...";  
+    digitalWrite(ledPin, LOW);
+    delay(door_delay);
+    digitalWrite(ledPin, HIGH); 
   }
   else
   {
     message = "Wrong API key";
   }
 }
-server.send(200, “text/plain”, message);          //Returns the HTTP response
+server.send(200, "text/plain", message);          //Returns the HTTP response
 
 }
